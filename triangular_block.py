ed4e220a67bccea2f02ed0266405cdcc27163318
jgd10 <jgd10@imperial.ac.uk>
2016-02-23 20:28:21 +0000

    touched up triangular_block

--- triangular_block.py
+++ triangular_block.py
@@ -7,41 +7,32 @@ import matplotlib.pyplot as plt
 import time
 
 
-X_cells    = 1000 
-Y_cells    = 1000 
-x_length   = 500.e-6
-y_length   = 500.e-6
-GRIDSPC    = x_length/X_cells
+L_cells    = 500 				# T - Transverse, L - Longitudinal
+T_cells    = 200 
+T_length   = 200.e-6
+L_length   = 500.e-6
+GRIDSPC    = L_length/L_cells
 
-pss.generate_mesh(X_cells,Y_cells,mat_no=1)
+pss.generate_mesh(L_cells,T_cells,mat_no=1)
 mats = pss.mats
 
-r1 = np.array([0.,67.5])   # in microns
-r2 = np.array([100.,92.5])
-r3 = np.array([200.,67.5])
-r4 = np.array([300.,92.5])
-r5 = np.array([400.,67.5])
-r6 = np.array([500.,92.5])
+r1 = np.array([0.,92.5])
+r2 = np.array([100.,67.5])   # in microns
+r3 = np.array([200.,92.5])
+#r4 = np.array([300.,67.5])   # in microns
+#r5 = np.array([400.,92.5])
+#r6 = np.array([500.,67.5])   # in microns
 
 r1 = r1[::-1]
 r2 = r2[::-1]
 r3 = r3[::-1]
-r4 = r4[::-1]
-r5 = r5[::-1]
-r6 = r6[::-1]
 
 r1 /= GRIDSPC*1.e6
 r2 /= GRIDSPC*1.e6
 r3 /= GRIDSPC*1.e6
-r4 /= GRIDSPC*1.e6
-r5 /= GRIDSPC*1.e6
-r6 /= GRIDSPC*1.e6
 
 pss.fill_above_line(r1,r2,mats[0],mixed=True)
 pss.fill_above_line(r2,r3,mats[0],mixed=True)
-pss.fill_above_line(r3,r4,mats[0],mixed=True)
-pss.fill_above_line(r4,r5,mats[0],mixed=True)
-pss.fill_above_line(r5,r6,mats[0],mixed=True)
 
 #pss.fill_plate(0.,300.,mats[1])
 
