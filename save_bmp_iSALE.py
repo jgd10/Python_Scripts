a1e0902cb99df929b0feb3d85e96ca3db9b308a6
jgd10 <jgd10@imperial.ac.uk>
2016-02-24 18:33:02 +0000

    added file to save a bitmap as meso_m.iSALE

---
+++ save_bmp_iSALE.py
@@ -0,0 +1,20 @@
+import pySALESetup as pss
+import numpy as np
+from PIL import Image
+import sys
+import matplotlib.pyplot as plt
+
+im = Image.open(sys.argv[1])
+
+A = np.asarray(im)
+B = np.copy(A)
+B[B>0.] = 1.
+
+print np.shape(B)
+
+pss.populate_from_bmp(B)
+"""
+plt.figure()
+plt.imshow(B,interpolation='nearest')
+plt.show()
+"""
