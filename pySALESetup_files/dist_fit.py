27cda878a6795d1be18e56996ea9e94edac2bb61
jgd10 <jgd10@imperial.ac.uk>
2016-11-28 12:04:05 +0000

    distribution fitter script. fits CDF to data

--- pySALESetup_files/dist_fit.py
+++ pySALESetup_files/dist_fit.py
@@ -13,17 +13,23 @@ B = .5
 C = 4.665
 L = .7
 
+
 phi = np.linspace(-2,9,100)
 popt, pcov = curve_fit(function,data[:,0],data[:,1],p0 = [A,B,C,L])
 print 'fit: A = {:3.3f}, B = {:3.3f}, C = {:3.3f}, L = {:3.3f}'.format(popt[0],popt[1],popt[2],popt[3])
-NORM = function(9.,A,B,C,L)-function(-2.,A,B,C,L)
+NORM = function(9.,popt[0],popt[1],popt[2],popt[3])-function(-2.,popt[0],popt[1],popt[2],popt[3])
 pdf  = A*L*C*np.exp(L*phi)/(B*np.exp(L*phi)+C)**2.
 pdf /= NORM
+print 'Norm factor = {}'.format(NORM)
+
+
+D = 2**(-phi)
+D*= 1000
 
 plt.figure()
 plt.plot(data[:,0],data[:,1],marker='o', linestyle=' ')
-plt.plot(phi,function(phi,popt[0],popt[1],popt[2],popt[3]))
+plt.plot(D,function(phi,popt[0],popt[1],popt[2],popt[3]))
 
 plt.figure()
-plt.plot(phi,pdf)
+plt.plot(D,pdf)
 plt.show()
