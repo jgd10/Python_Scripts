e0500b59799a7342194bb938d56e6063fbb15544
jgd10 <jgd10@imperial.ac.uk>
2017-02-17 11:02:50 +0000

    minor text fixes

--- pySALESetup_files/save_bmp_iSALE.py
+++ pySALESetup_files/save_bmp_iSALE.py
@@ -8,13 +8,12 @@ im = Image.open(sys.argv[1])
 
 A = np.asarray(im)
 B = np.copy(A)
-B[B>0.] = 1.
+#B[B>0.] = 1.
 
+B = B[:,::-1]
+B = B.transpose()
 print np.shape(B)
-
 pss.populate_from_bmp(B)
-"""
 plt.figure()
 plt.imshow(B,interpolation='nearest')
 plt.show()
-"""
